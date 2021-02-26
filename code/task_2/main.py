f1806824ce3c5b34123d84eb4e824ec9409ac991
Archan Lagoo <aklagoo@gmail.com>
2021-02-26 13:56:43 -0700

    Added comments to task2/main.py and utils.py

--- code/task_2/main.py
+++ code/task_2/main.py
@@ -6,56 +6,95 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-def load_img(img_dir=const.DIR_IMG):
-    img_l = cv2.imread(os.path.join(img_dir, 'task_2\\left_0.png'))
-    img_r = cv2.imread(os.path.join(img_dir, 'task_2\\right_0.png'))
+def load_img(dir_img: str = const.DIR_IMG, indices: tuple = (0, 0)):
+    """Loads chessboard images from left and right cameras.
+
+    Args:
+        dir_img: Path to the image directory
+        indices: Indices to select left and right images
+
+    Returns:
+        Two NumPy arrays containing image data
+    """
+    img_l = cv2.imread(os.path.join(dir_img, 'task_2\\left_{0}.png'.format(indices[0])))
+    img_r = cv2.imread(os.path.join(dir_img, 'task_2\\right_{0}.png'.format(indices[1])))
 
     return img_l, img_r
 
 
-def load_params(params_dir=const.DIR_PARAMS):
-    cam_mtx_l = np.loadtxt(os.path.join(params_dir, 'left_camera_intrinsics_cam_mtx.csv'))
-    dst_l = np.loadtxt(os.path.join(params_dir, 'left_camera_intrinsics_dst.csv'))
-    cam_mtx_r = np.loadtxt(os.path.join(params_dir, 'right_camera_intrinsics_cam_mtx.csv'))
-    dst_r = np.loadtxt(os.path.join(params_dir, 'right_camera_intrinsics_dst.csv'))
+def load_intrinsics(dir_params: str = const.DIR_PARAMS):
+    """Loads intrinsics for left and right cameras from files.
+
+    Args:
+        dir_params: Path to the intrinsics/parameters directory
+
+    Returns:
+        Four NumPy arrays containing camera intrinsic matrices and distortion
+        coefficients for each camera in the following order:
+
+        `left camera matrix, left distortion coefficients, right camera matrix,
+        right distortion coefficients`
+    """
+    cam_mtx_l = np.loadtxt(os.path.join(dir_params, 'left_camera_intrinsics_cam_mtx.csv'))
+    dst_l = np.loadtxt(os.path.join(dir_params, 'left_camera_intrinsics_dst.csv'))
+    cam_mtx_r = np.loadtxt(os.path.join(dir_params, 'right_camera_intrinsics_cam_mtx.csv'))
+    dst_r = np.loadtxt(os.path.join(dir_params, 'right_camera_intrinsics_dst.csv'))
 
     return cam_mtx_l, dst_l, cam_mtx_r, dst_r
 
 
-def stereo_calibrate(img, params):
+def stereo_calibrate(img, intrinsics):
+    """Performs camera calibration and returns rotation, translation, fundamental and essential matrices"""
+    # Extract data from arguments
     img_l, img_r = img
-    cam_mtx_l, dst_l, cam_mtx_r, dst_r = params
+    cam_mtx_l, dst_l, cam_mtx_r, dst_r = intrinsics
 
+    # Find corners from left and right images
     crn_l = cv2.findChessboardCorners(img_l, const.SIZE_GRID)[1]
     crn_r = cv2.findChessboardCorners(img_r, const.SIZE_GRID)[1]
 
+    # Generate 3D object points corresponding to chessboard corners
     obj_pt = np.concatenate((
         np.mgrid[0:const.SIZE_GRID[0], 0:const.SIZE_GRID[1]].T.reshape(-1, 2),
         np.zeros((const.SIZE_GRID[0] * const.SIZE_GRID[1], 1))
     ), axis=1).astype('float32')
 
-    params = cv2.stereoCalibrate(np.array([obj_pt]), np.array([crn_l]), np.array([crn_r]), cam_mtx_l, dst_l, cam_mtx_r,
-                                 dst_r, (480, 640), flags=cv2.CALIB_FIX_INTRINSIC)
-    _, cam_mtx_l, dst_l, cam_mtx_r, dst_r, R, T, E, F = params
+    # Perform stereo calibration and extract rotation, translation,
+    # essential and fundamental matrices
+    _params = cv2.stereoCalibrate(
+                np.array([obj_pt]), np.array([crn_l]), np.array([crn_r]),
+                cam_mtx_l, dst_l, cam_mtx_r, dst_r, (480, 640),
+                flags=cv2.CALIB_FIX_INTRINSIC)
+    _, cam_mtx_l, dst_l, cam_mtx_r, dst_r, R, T, E, F = _params
 
+    # In the following section, we verify the calibration by
+    # triangulating and plotting these points in 3D. We begin by
+    # undistorting the extracted corners.
+    #
+    # To triangulate these points, we generate projection matrices [I|0]
+    # and [R|t]. We then pass these matrices and undistorted points to
+    # cv2.triangulatePoints to get homogeneous points. These points are
+    # converted to cartesian points and plotted on a graph with a camera.
     undist_crn_l = cv2.undistortPoints(crn_l, cam_mtx_l, dst_l)
     undist_crn_r = cv2.undistortPoints(crn_r, cam_mtx_r, dst_r)
-
     projection_l = np.concatenate((np.identity(3), np.zeros((3, 1))), axis=1).astype('float32')
     projection_r = np.concatenate((R, T), axis=1).astype('float32')
 
+    # Triangulate points and convert to cartesian points
     points = cv2.triangulatePoints(projection_l, projection_r, undist_crn_l, undist_crn_r)
     cartesian_points = points[:3] / points[3]
 
+    # Create subplot and plot cameras and points
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(*cartesian_points)
     utils.plot_camera(ax)
     utils.plot_camera(ax, R, T)
-
     plt.show()
 
-img = load_img()
-params = load_params()
 
-stereo_calibrate(img, params)
+if __name__ == '__main__':
+    images = load_img()
+    parameters = load_intrinsics()
+
+    stereo_calibrate(images, parameters)
--- utils.py
+++ utils.py
@@ -1,11 +1,24 @@
 import numpy as np
+import math
 
 
-def plot_camera(ax, r=np.identity(3), t=np.zeros((3, 1))):
-    # Constants assumed
-    tan_x, tan_y, f = 1, 1, 1
+def plot_camera(ax, r=np.identity(3), t=np.zeros((3, 1)), theta_x=45, theta_y=45, f=1):
+    """Draws camera in 3D space.
 
-    points = np.asarray([
+    Args:
+        ax: A 3D matplotlib subplot
+        r: A rotation matrix. Defaults to an identity matrix for no rotation
+        t: A translation matrix. Defaults to zero for no translation
+        theta_x: Horizontal field of view
+        theta_y: Vertical field of view
+        f: Focal length of camera
+    """
+    # Calculate tan(x) and tan(y)
+    tan_x = math.tan(math.radians(theta_x))
+    tan_y = math.tan(math.radians(theta_y))
+
+    # Camera polygon vertices
+    vertices = np.asarray([
         (0, 0, 0),
         (-tan_x, -tan_y, 1),
         (0, 0, 0),
@@ -19,7 +32,8 @@ def plot_camera(ax, r=np.identity(3), t=np.zeros((3, 1))):
         (-tan_x, tan_y, 1),
         (tan_x, tan_y, 1)
     ]) * f
+    # Rotate and translate camera
+    vertices = np.dot(vertices, r).T + t
 
-    points = np.dot(points, r).T + t
-
-    ax.plot(*points, color='black')
+    # Draw camera polygon
+    ax.plot(*vertices, color='black')
